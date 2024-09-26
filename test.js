107d472c37960a6bd632427e89b60ca9e5f7258f
Alexandre Estapé <aestape2002@gmail.com>
2024-09-26 11:38:32 +0200

    UPDATE-Added .js test document

---
+++ test.js
@@ -0,0 +1,40 @@
+const myObject = {
+  level1: {
+    item1: {
+      key1: "string1",
+      key2: "string2",
+      key3: "string3",
+    },
+    item2: {
+      key1: "string4",
+      key2: "string5",
+      key3: "string6",
+    },
+  },
+  level2: {
+    item1: {
+      key1: "string7",
+      key2: "string8",
+      key3: "string9",
+    },
+    item2: {
+      key1: "string10",
+      key2: "string11",
+      key3: "string12",
+    },
+  },
+  level3: {
+    item1: {
+      key1: "string13",
+      key2: "string14",
+      key3: "string15",
+    },
+    item2: {
+      key1: "string16",
+      key2: "string17",
+      key3: "string18",
+    },
+  },
+};
+
+console.log(myObject);
